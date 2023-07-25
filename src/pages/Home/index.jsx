0a71a6b70c83d09ccdaedf6b30b8db4608a903ec
Eric Oliveira <oiericoi@hotmail.com>
2023-07-25 15:21:18 -0300

    refactor: modify home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,11 @@
 import Footer from "../../components/Footer"
-import HeaderHome from "../../components/HeaderHome"
+import Header from "../../components/Header"
 import Main from "../../components/Main"
 
 const Home = () => {
   return (
     <>
-      <HeaderHome />
+      <Header />
       <Main />
       <Footer />
     </>
