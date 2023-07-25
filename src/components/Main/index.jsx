3d41a5f740528a74706e5ec58f6d3385b68953ed
Eric Oliveira <oiericoi@hotmail.com>
2023-07-25 11:56:49 -0300

    feat: add main component

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+const Main = () => {
+  return (
+    <div className="main-container">
+      <h1 id="main-title">Organize-se</h1>
+      <h3 id="main-subtitle">Seja mais produtivo: crie, acompanhe e conclua tarefas com nosso app.</h3>
+    </div>
+  )
+}
+
+export default Main
