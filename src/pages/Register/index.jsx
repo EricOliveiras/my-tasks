81fabc2e19f93c2522a80b9377deaee4322d28fd
Eric Oliveira <oiericoi@hotmail.com>
2023-07-25 15:21:37 -0300

    feat: add register page

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header"
+import RegisterForm from "../../components/RegisterForm"
+import Footer from "../../components/Footer"
+
+const Register = () => {
+  return (
+    <>
+      <Header />
+      <RegisterForm />
+      <Footer />
+    </>
+  )
+}
+
+export default Register
