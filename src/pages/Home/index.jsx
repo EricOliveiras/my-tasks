065dc9763def483a946b126a18f452c53db4a60f
Eric Oliveira <oiericoi@hotmail.com>
2023-07-25 11:58:25 -0300

    feat: add home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import Footer from "../../components/Footer"
+import HeaderHome from "../../components/HeaderHome"
+import Main from "../../components/Main"
+
+const Home = () => {
+  return (
+    <>
+      <HeaderHome />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
