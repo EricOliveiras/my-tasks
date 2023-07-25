2ed7ed37901140bac50656823c45a56c0e696ad1
Eric Oliveira <oiericoi@hotmail.com>
2023-07-25 15:22:32 -0300

    feat: import register and login pages in app router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from '../pages/Home'
+import Register from "../pages/Register"
+import Login from "../pages/Login"
 
 const AppRouter = () => {
   return (
@@ -7,6 +9,8 @@ const AppRouter = () => {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />}/>
+          <Route exact path="/registrar" element={<Register />}/>
+          <Route exact path="/entrar" element={<Login />}/>
         </Routes>
       </BrowserRouter>
     </>
