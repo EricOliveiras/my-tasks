06b50a8330d3e855c16088c72199367a1aef92f4
Eric Oliveira <oiericoi@hotmail.com>
2023-08-01 14:52:50 -0300

    feat: add dashboard page in router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 import Home from '../pages/Home'
 import Register from "../pages/Register"
 import Login from "../pages/Login"
+import Dashboard from "../pages/Dashboard"
+import PrivateRoute from "../components/PrivateRoute"
 
 const AppRouter = () => {
   return (
@@ -11,6 +14,11 @@ const AppRouter = () => {
           <Route exact path="/" element={<Home />}/>
           <Route exact path="/registrar" element={<Register />}/>
           <Route exact path="/entrar" element={<Login />}/>
+          <Route path="/dashboard" element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }/>
         </Routes>
       </BrowserRouter>
     </>
