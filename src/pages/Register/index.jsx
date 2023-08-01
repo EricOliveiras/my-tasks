529932c76d14ba2aa6ac6a507b1242bb79a4c6da
Eric Oliveira <oiericoi@hotmail.com>
2023-08-01 14:51:13 -0300

    refactor: modify register page

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,12 +1,13 @@
-import Header from "../../components/Header"
-import RegisterForm from "../../components/RegisterForm"
-import Footer from "../../components/Footer"
 import { ToastContainer } from "react-toastify"
+
+import RegisterForm from "../../components/RegisterForm"
+import Header from "../../components/Header"
+import Footer from "../../components/Footer"
 
 const Register = () => {
   return (
     <>
-      <Header />
+      <Header headerTitle={"My Tasks"}/>
       <RegisterForm />
       <Footer />
       <ToastContainer />
