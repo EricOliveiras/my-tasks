4bab6f6c1fe7753699b53e8dd74cb1bfbfa0a0cc
Eric Oliveira <oiericoi@hotmail.com>
2023-08-01 14:44:49 -0300

    refactor: modify header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,20 @@
 import { Link, useLocation } from "react-router-dom"
-import Button from '../button'
+import PropTypes from "prop-types"
 
-import './style.css'
+import Button from "../button"
 
-const Header = () => {
+import "./style.css"
+
+// eslint-disable-next-line react/prop-types
+const Header = ({ headerTitle }) => {
   const location = useLocation()
 
-  let buttonComponent;
+  const clearToken = () => {
+    localStorage.removeItem("token")
+  }
+
+  let buttonComponent
+  let path = "/"
 
   if (location.pathname === "/") {
     buttonComponent =               
@@ -17,19 +25,31 @@ const Header = () => {
   } else if (location.pathname === "/registrar" || location.pathname === "/entrar") {
     buttonComponent = <Button path="/" btnId="register-button" name="Voltar" />
   } else {
-    buttonComponent = <Button path="/" btnId="register-button" name="Dashboard" />
+    buttonComponent = 
+    <>
+      <Button path="/perfil" btnId="register-button" name="Perfil" />
+      <Button path="/" btnId="register-button" name="Sair" click={clearToken}/>
+    </>
   }
+
+  if (location.pathname === "/dashboard") {
+    path = "/dashboard"
+  } 
 
   return (
     <>
       <div className="header-container">
-        <Link id="header-title" to={"/"}><h2>My Tasks</h2></Link>
+        <Link id="header-title" to={path}><h2>{headerTitle}</h2></Link>
         <div className="button-container">
           {buttonComponent}
         </div>
       </div>
     </>
   )
+}
+
+Header.PropTypes = {
+  headerTitle: PropTypes.string.isRequired,
 }
 
 export default Header
