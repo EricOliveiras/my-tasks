9c06a3b3236c1221bb06e7cd387594153e1102ad
Eric Oliveira <oiericoi@hotmail.com>
2023-08-01 14:52:07 -0300

    feat: add dashboard page

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../components/Footer"
+import UserDashboard from "../../components/UserDashboard";
+
+const Dashboard = () => {
+  return (
+    <>
+      <UserDashboard />
+      <Footer />
+    </>
+  )
+}
+
+export default Dashboard
