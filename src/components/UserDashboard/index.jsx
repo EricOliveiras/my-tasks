aad2e50d6ba163b38ad5a6d08a0ceb7146f627c6
Eric Oliveira <oiericoi@hotmail.com>
2023-08-02 18:46:50 -0300

    refactor: modify user dashboard component

--- src/components/UserDashboard/index.jsx
+++ src/components/UserDashboard/index.jsx
@@ -1,37 +1,107 @@
 import { useEffect, useState } from "react"
+import { ToastContainer } from "react-toastify"
 
 import { readUser } from "../../service/user"
+import { createTask, deleteTask, finishTask, updateteTask } from "../../service/task"
 import TaskCard from "../TaskCard"
 import Header from "../Header"
-import Button from "../button"
+import Modal from "../Modal"
+import TaskForm from "../TaskForm"
 
 import "./style.css"
 
 const UserDashboard = () => {
   const [user, setUser] = useState([])
-  const [task, seTask] = useState()
+  const [task, setTask] = useState([])
+  const [isOpen, setIsOpen] = useState(false)
+  const [isUpdateOpen, setIsUpdateOpen] = useState(false)
+  const [selectedTask, setSelectedTask] = useState({})
 
-  const handleFinishTask = (taskId) => {
-    // Lógica para marcar a tarefa como finalizada
-    console.log(`Tarefa ${taskId} finalizada.`);
+  const handleOpenModal = () => {
+    setIsOpen(true)
+  }
+
+  const handleCloseModal = () => {
+    setIsOpen(false)
+  }
+
+  const handleOpenUpdateModal = (task) => {
+    setSelectedTask({ id: task.id, title: task.title, description: task.description })
+    setIsUpdateOpen(true)
   };
 
-  const handleUpdateTask = (taskId) => {
-    // Lógica para atualizar a tarefa
-    console.log(`Atualizar tarefa ${taskId}.`);
+  const handleCloseUpdateModal = () => {
+    setSelectedTask(null)
+    setIsUpdateOpen(false)
   };
+  
+  const handleAddTask = async ({ title, description }) => {
+    const token = localStorage.getItem("token")
+    const response = await createTask(token, title, description)
 
-  const handleDeleteTask = (taskId) => {
-    // Lógica para excluir a tarefa
-    console.log(`Excluir tarefa ${taskId}.`);
-  };
+    if (!response) return
+    
+    setTask([...task, response.data.data])
+
+    handleCloseModal()
+  }
+
+  const handleFinishTask = async (task_id) => {
+    const confirmed = window.confirm("Tem certeza de que deseja finalizar esta tarefa?")
+    if (!confirmed) return
+
+    const token = localStorage.getItem("token")
+
+    const response = await finishTask(token, task_id)
+
+    if (!response) return
+
+    setTask((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === task_id ? { ...task, finished: true } : task
+      )
+    )
+  }
+
+  const handleUpdateTask = async ({title, description}) => {
+    const token = localStorage.getItem("token")
+
+    const { id } = selectedTask
+
+    const response = await updateteTask(token, id, title, description)
+
+    if (!response) return
+
+    setTask((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, title, description } : task
+      )
+    )
+
+    handleCloseUpdateModal()
+  }
+
+  const handleDeleteTask = async (task_id) => {
+    const confirmed = window.confirm("Tem certeza de que deseja excluir esta tarefa?")
+    if (!confirmed) return
+
+    const token = localStorage.getItem("token")
+
+    const response = await deleteTask(token, task_id)
+
+    if (!response) return
+
+    const updatedTasks = task.filter((task) => task.id !== task_id)
+    setTask(updatedTasks)
+  }
   
   useEffect(() => {
     const getUser = async () => {
       const token = localStorage.getItem("token")
       const response = await readUser(token)
+      console.log(response.data.Task)
       setUser(response.data)
-      seTask(response.data.Task)
+      setTask(response.data.Task)
     }
     getUser()
   }, [])
@@ -43,24 +113,34 @@ const UserDashboard = () => {
       <div className="user-tasks-container">
         <div className="task-info">
           <h3>Suas tarefas:</h3>
-          <Button path="/create-task" btnId="new-task-button" name="Nova Tarefa"/>
+          <button id="new-task-button" type="submit" onClick={handleOpenModal}>Nova Tarefa</button>
+          <Modal isOpen={isOpen} onClose={handleCloseModal}>
+            <TaskForm onSubmit={handleAddTask} typeForm={"Criar Nova Tarefa"}/>
+          </Modal>
+          <Modal isOpen={isUpdateOpen} onClose={handleCloseUpdateModal}>
+            {selectedTask && (
+              <TaskForm onSubmit={handleUpdateTask} typeForm={"Atualizar Tarefa"} initialValues={selectedTask}/>
+            )}
+          </Modal>
         </div>
         <div className="task-container">
           <h3>Pendentes:</h3>
           <div className="task-list">
             {task && (task.map((pending) => (
               !pending.finished && (
+              <>
                 <TaskCard
                   key={pending.id}
                   title={pending.title}
                   description={pending.description}
                   finished={pending.finished}
                   onFinish={() => handleFinishTask(pending.id)}
-                  onUpdate={() => handleUpdateTask(pending.id)}
+                  onUpdate={() => handleOpenUpdateModal(pending)}
                   onDelete={() => handleDeleteTask(pending.id)}
                 />
-              )
-            )))}
+              </>
+              ))
+            ))}
           </div>
           <div className="task-container">
             <h3>Finalizadas:</h3>
@@ -82,6 +162,7 @@ const UserDashboard = () => {
           </div>
         </div>
       </div>
+      <ToastContainer />
     </>
   )
 }
