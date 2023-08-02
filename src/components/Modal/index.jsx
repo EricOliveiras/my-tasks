789bba78fe36af4f3b9fb1473adbf4fc901056a0
Eric Oliveira <oiericoi@hotmail.com>
2023-08-02 18:43:10 -0300

    feat: add modal component

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types"
+
+import "./style.css"
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="modal-close" onClick={onClose}>
+          Fechar
+        </button>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+}
+
+export default Modal
