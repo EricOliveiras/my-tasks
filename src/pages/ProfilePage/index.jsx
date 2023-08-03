e5dad5302a9d3adf5c31977476803c41657c0427
Eric Oliveira <oiericoi@hotmail.com>
2023-08-03 13:15:55 -0300

    feat: add profile page

---
+++ src/pages/ProfilePage/index.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header"
+import Profile from "../../components/Profile"
+
+const ProfilePage = () => {
+  return (
+    <>
+      <Header />
+      <Profile />
+    </>
+  )
+}
+
+export default ProfilePage
